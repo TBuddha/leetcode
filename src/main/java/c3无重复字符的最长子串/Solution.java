68e4379d5f53d465a2eaa4bd8c22c6e5f028b6ea
zhout <zhoutao@yunjiacloud.com>
2020-05-19 16:25:58 +0800

    3.无重复字符的最长子串

---
+++ src/main/java/c3无重复字符的最长子串/Solution.java
@@ -0,0 +1,38 @@
+package c3无重复字符的最长子串;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 官解，思路是：滑动窗口
+ * https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/solution/wu-zhong-fu-zi-fu-de-zui-chang-zi-chuan-by-leetc-2/
+ *
+ * 时间复杂度：O(N)
+ * 空间复杂度：O(∣Σ∣)，其中 Σ 表示字符集（即字符串中可以出现的字符）
+ *
+ * @author zhout
+ * @date 2020/5/19 15:03
+ */
+class Solution {
+  public static int lengthOfLongestSubstring(String s) {
+    // 哈希集合，记录每个字符是否出现过
+    Set<Character> occ = new HashSet<>();
+    int n = s.length();
+    // 右指针，初始值为 -1，相当于我们在字符串的左边界的左侧，还没有开始移动
+    int rk = -1, ans = 0;
+    for (int i = 0; i < n; ++i) {
+      if (i != 0) {
+        // 左指针向右移动一格，移除一个字符
+        occ.remove(s.charAt(i - 1));
+      }
+      while (rk + 1 < n && !occ.contains(s.charAt(rk + 1))) {
+        // 不断地移动右指针
+        occ.add(s.charAt(rk + 1));
+        ++rk;
+      }
+      // 第 i 到 rk 个字符是一个极长的无重复字符子串
+      ans = Math.max(ans, rk - i + 1);
+    }
+    return ans;
+  }
+}
